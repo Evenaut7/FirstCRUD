507023b9fb101dd06d50d2ed54063efad43c6e84
Evenaut7 <valentinolaveggi@outlook.com>
2025-05-26 08:32:42 -0300

    First version of FistCRUD finished

--- src/app.ts
+++ src/app.ts
@@ -26,11 +26,11 @@ function sanitizeTagInput(req: Request, res: Response, next: NextFunction){
   }
   //- Agregar Validaciones necesarias
 
-    Object.keys(req.body.sanitizedInput).forEach(key => {
-      if(req.body.sanitizedInput[key] === undefined){
-        delete req.body.sanitizedInput[key];
-      }
-    });
+  Object.keys(req.body.sanitizedInput).forEach(key => {
+    if(req.body.sanitizedInput[key] === undefined){
+      delete req.body.sanitizedInput[key];
+    }
+  });
   next();
 }
 
@@ -44,8 +44,11 @@ app.get('/api/tags', (req, res) => {
 // GetOne()
 app.get('/api/tags/:id', (req, res) => {
   const aTag = tags.find(tag => tag.id === req.params.id);
-  if(!aTag) res.status(404).send({message: 'Tag not found'});
-  res.json(aTag);
+  if(!aTag) {
+    res.status(404).send({message: 'Tag not found'});
+    return;
+  }
+  res.json({data: aTag});
 });
 
 
@@ -66,6 +69,7 @@ app.put('/api/tags/:id', sanitizeTagInput, (req, res) => {
   
   if(tagIdx === -1) {
     res.status(404).send({message: 'Tag not found'});
+    return;
   }
   
   tags[tagIdx] = {...tags[tagIdx], ...req.body.sanitizedInput}
@@ -80,6 +84,7 @@ app.patch('/api/tags/:id', sanitizeTagInput, (req, res) => {
   
   if(tagIdx === -1) {
     res.status(404).send({message: 'Tag not found'});
+    return;
   }
   
   tags[tagIdx] = {...tags[tagIdx], ...req.body.sanitizedInput}
@@ -94,12 +99,17 @@ app.delete('/api/tags/:id', (req, res) => {
 
   if (tagIdx === -1){
     res.status(400).send({message: 'Tag not found'})
-  }
+  } else {
   tags.splice(tagIdx, 1);
-  res.status(200).send({message: 'Tag deleted succesfully'})
+  res.status(200).send({message: 'Tag deleted succesfully'});
+  }
 
 });
 
+app.use((_,res) => {
+  res.status(404).send({message: 'Resource not found'});
+  return;
+})
 
 // Start
 app.listen(3000, () => {
